7b4338f82ea7aa41f7284f47ff88a93566bda6d3
Zhaoyu Wei <weixiaobo88@gmail.com>
2014-08-18 17:28:02 +0800

    zy: change the algorithm of counting

--- src/main/java/CompareNumber.java
+++ src/main/java/CompareNumber.java
@@ -1,28 +1,26 @@
 public class CompareNumber {
-    public int countA(String challenge, String guess) {
+    public int countA(String answer, String input) {
         int count = 0;
-        for (int i = 0; i < challenge.length(); i++) {
-            if (challenge.charAt(i) == guess.charAt(i)) {
+        for (int i = 0; i < answer.length(); i++) {
+            if (answer.charAt(i) == input.charAt(i)) {
                 count += 1;
             }
         }
         return count;
     }
 
-    public int countBIncludingA(String challenge, String guess) {
+    public int countBIncludingA(String answer, String input) {
         int count = 0;
-        for (int i = 0; i < guess.length(); i++) {
-            for (int j = 0; j < challenge.length(); j++) {
-                if (guess.charAt(i) == challenge.charAt(j)) {
-                    count += 1;
-                }
+        for (int index = 0; index < answer.length(); index++) {
+            if (answer.indexOf(input.charAt(index)) != -1) {
+                count += 1;
             }
         }
         return count;
     }
 
     public String compare(String answer, String input) {
-            int count_a = countA(answer, input);
+        int count_a = countA(answer, input);
         int count_b_with_a = countBIncludingA(answer, input);
 
         return count_a + "A" + (count_b_with_a - count_a) + "B";
